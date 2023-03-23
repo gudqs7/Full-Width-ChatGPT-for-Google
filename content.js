2f18acbe35ff6b7732a96575ba9811de5f214300
gudqs7 <guddqs@icloud.com>
2023-03-23 22:09:44 +0800

    up

--- content.js
+++ content.js
@@ -3,18 +3,17 @@ var timer = setInterval(() => {
   fireContentLoadedEvent()
 }, 300);
 
-function fireContentLoadedEvent () {
-    console.log('fireContentLoadedEvent wq')
-
-    var c4f = document.querySelector('#c4g-search-enhance')
-    if (c4f) {
-      console.log('c4f wq')
-        document.querySelector('#c4g-search-enhance').shadowRoot.querySelector('div > div').style="width: 701px;"
-        
-        var root = document.querySelector('.srp')
-        root.style.setProperty('--rhs-width', '772px');
-
-        clearInterval(timer)
+function fireContentLoadedEvent() {
+  var c4f = document.querySelector('#c4g-search-enhance')
+  if (c4f) {
+    var c4gSearch = document.querySelector('#c4g-search-enhance')
+    if (c4gSearch) {
+      c4gSearch.shadowRoot.querySelector('div > div').style = "width: 701px;"
     }
-
+    var root = document.querySelector('.srp')
+    if (root) {
+      root.style.setProperty('--rhs-width', '772px');
+    }
+    clearInterval(timer)
+  }
 }
