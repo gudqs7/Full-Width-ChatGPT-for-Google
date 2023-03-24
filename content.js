c9013a097f8159ff9d48f5ad6a5dfb62f902fe5f
gudqs7 <guddqs@icloud.com>
2023-03-24 16:08:45 +0800

    add icon

--- content.js
+++ content.js
@@ -4,16 +4,21 @@ var timer = setInterval(() => {
 }, 300);
 
 function fireContentLoadedEvent() {
+  console.log('sorry wq')
   var c4f = document.querySelector('#c4g-search-enhance')
   if (c4f) {
     var c4gSearch = document.querySelector('#c4g-search-enhance')
     if (c4gSearch) {
-      c4gSearch.shadowRoot.querySelector('div > div').style = "width: 701px;"
+      var searchShadowRoot = c4gSearch.shadowRoot
+      if (searchShadowRoot && searchShadowRoot.querySelector('div > div')) {
+        searchShadowRoot.querySelector('div > div').style = "width: 701px;"
+        clearInterval(timer)
+
+        var root = document.querySelector('.srp')
+        if (root) {
+          root.style.setProperty('--rhs-width', '772px');
+        }
+      }
     }
-    var root = document.querySelector('.srp')
-    if (root) {
-      root.style.setProperty('--rhs-width', '772px');
-    }
-    clearInterval(timer)
   }
 }
