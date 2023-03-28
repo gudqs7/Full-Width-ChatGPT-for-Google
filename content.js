27f122bfe7d12c75854bb13ef5373da55c85792a
gudqs7 <guddqs@icloud.com>
2023-03-28 15:12:53 +0800

    rm ads

--- content.js
+++ content.js
@@ -1,25 +1,26 @@
 var afterWidth = 700
 
 var timer = setInterval(() => {
-  fireContentLoadedEvent()
+    fireContentLoadedEvent()
 }, 300);
 
 function fireContentLoadedEvent() {
-  console.log('sorry wq')
-  var c4f = document.querySelector('#c4g-search-enhance')
-  if (c4f) {
+    console.log('sorry wq')
     var c4gSearch = document.querySelector('#c4g-search-enhance')
     if (c4gSearch) {
-      var searchShadowRoot = c4gSearch.shadowRoot
-      if (searchShadowRoot && searchShadowRoot.querySelector('div > div')) {
-        searchShadowRoot.querySelector('div > div').style = "width: "+ afterWidth +"px;"
-        clearInterval(timer)
+        var searchShadowRoot = c4gSearch.shadowRoot
+        if (searchShadowRoot && searchShadowRoot.querySelector('div > div')) {
+            searchShadowRoot.querySelector('div > div').style = "width: " + afterWidth + "px;"
+            clearInterval(timer)
 
-        var root = document.querySelector('.srp')
-        if (root) {
-          root.style.setProperty('--rhs-width', (afterWidth + 20) + 'px');
+            var root = document.querySelector('.srp')
+            if (root) {
+                root.style.setProperty('--rhs-width', (afterWidth + 20) + 'px');
+            }
+
+            var style = document.createElement('style');
+            style.innerHTML = '.promotion-3PDMAb {visibility: hidden!important;}';
+            searchShadowRoot.appendChild(style);
         }
-      }
     }
-  }
 }
